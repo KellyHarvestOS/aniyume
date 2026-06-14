41a2c20838c2ed3ae28f19ed650781984c89065d
KellyHarvestOS <vladjjjsss7@gmail.com>
2026-06-15 00:58:24 +0500

    fix(Header): simplify chat icon className formatting

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -232,11 +232,7 @@ export default function Header() {
               >
                 <HiOutlineChatBubbleOvalLeftEllipsis
                   size={26}
-                  className={`transition-colors ${isChatActive
-                      ? 'text-brand'
-                      : 'text-gray-400 dark:text-gray-500 group-hover:text-gray-900 dark:group-hover:text-gray-200'
-                    }`}
-                  style={{ fill: 'none' }}
+                  className={`transition-colors ${isChatActive ? 'text-brand' : 'text-gray-400 dark:text-gray-500 group-hover:text-gray-900 dark:group-hover:text-gray-200'}`}
                 />
 
                 <span className="absolute -top-1 -right-1 w-2 h-2 bg-red-500 rounded-full" />
